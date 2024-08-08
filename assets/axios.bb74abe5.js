22bf20d4294631ba2b8c473e1080431f841f07ac
Jerome Mare <jerome.mare@inetum.com>
2024-08-08 12:54:50 +0200

    Updates

--- assets/axios.bb74abe5.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index.e64b9411.js";import{a}from"./axios.d7524a27.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
